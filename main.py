34dd82ca194f36ea74656f16a8f6990e98855c53
scorpiomaster066-art <masterjustohero@gmail.com>
2026-07-23 14:23:26 -0600

    ui scroll fix botones

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-from pathlib import Path
+from kivy.metrics import dp
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
@@ -9,11 +8,30 @@ ScreenManager:
         md_bg_color: 0,0,0,1
         MDBoxLayout:
             orientation: 'vertical'
-            padding: dp(20)
-            MDLabel:
-                text: 'GestorMasterS NEGRA OK #32'
-                halign: 'center'
-                text_color: 1,1,1,1
+            MDTopAppBar:
+                title: "GestorMasterS"
+                pos_hint: {"top": 1}
+            ScrollView:
+                MDBoxLayout:
+                    orientation: 'vertical'
+                    padding: dp(15)
+                    spacing: dp(15)
+                    adaptive_height: True
+                    MDLabel:
+                        text: "GestorMasterS NEGRA OK FIJA"
+                        halign: "center"
+                        size_hint_y: None
+                        height: self.texture_size[1]
+                        text_color: 1,1,1,1
+                    MDRaisedButton:
+                        text: "Boton 1"
+                        pos_hint: {"center_x":.5}
+                    MDRaisedButton:
+                        text: "Boton 2"
+                        pos_hint: {"center_x":.5}
+                    MDRaisedButton:
+                        text: "Boton 3"
+                        pos_hint: {"center_x":.5}
 '''
 
 class GestorMasterSApp(MDApp):
