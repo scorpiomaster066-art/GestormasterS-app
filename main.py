a44a90224f99b78968a5ee69915b7ea7beed1636
scorpiomaster066-art <masterjustohero@gmail.com>
2026-07-23 01:17:58 -0600

    fix: restaura main.py negra #32

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-.	
-	
+import os
+from pathlib import Path
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
+KV = '''
+ScreenManager:
+    MDScreen:
+        md_bg_color: 0,0,0,1
+        MDBoxLayout:
+            orientation: 'vertical'
+            padding: dp(20)
+            MDLabel:
+                text: 'GestorMasterS NEGRA OK #32'
+                halign: 'center'
+                text_color: 1,1,1,1
+'''
+
+class GestorMasterSApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(KV)
+
+GestorMasterSApp().run()
